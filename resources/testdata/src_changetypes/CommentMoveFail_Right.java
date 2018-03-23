0a161dfb9daddbafaec861f14a3de86c49f2e3ed
nur-yilmaz <yilmazcnur@gmail.com>
2018-03-23 23:01:50 +0300

    COMMENT_MOVE unit test that fails added.

---
+++ resources/testdata/src_changetypes/CommentMoveFail_Right.java
@@ -0,0 +1,75 @@
+package test;
+
+/**
+ * This is our first (left) test class.
+ * @author Beat Fluri
+ */
+public class Test {
+
+	/*
+	 * Scarab Lord Kungen
+	 */
+	public String aField;
+
+	public static String sField;
+	public volatile int vField;
+	public transient String tField;
+	public synchronized long synchField;
+
+	private String arrayField;
+
+	public Integer anInteger = new Integer(1);
+
+	/**
+	 * Yet another method with a comment
+	 * @param number
+	 * @return
+	 */
+	public int foo(int number) {
+		System.out.println("left");
+		System.out.println("right");
+
+		// check if number is greater than -1
+		boolean check = number > 0;
+		int a = 0;
+		int b = 2;
+
+		// check the huga number
+		// and some new
+
+		if (check) {
+			/* huga bimbo */
+			/* This is the most beautiful comment in the world
+			 * and soon it will be gone :'(
+			 */
+			a = 23 + Integer.parseInt("42");
+			b = Math.round(Math.random() /* mimimi */);
+			return a + b;
+		} else {
+			b = Math.abs(number);
+			String.valueOf(true);
+			return b;
+		}
+	}
+	/*
+	 * Inner classes are cool
+	 */
+
+	private class Bar {
+		private void method() {
+			System.out.println();
+			System.out.println();
+			System.out.println();
+		}
+	}
+	// the huga bar method
+	public void bar(int test) {
+		System.out.println("aString");
+	}
+
+	public native void nativeMethod();
+
+	public strictfp float strictfpMethod() {
+		return 2.0f * 3.3f;
+	}
+}
