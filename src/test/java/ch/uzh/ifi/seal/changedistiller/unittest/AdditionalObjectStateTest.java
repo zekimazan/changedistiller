2834ec2165dcd9b232875586eb54735600c9d965
TheForty <musaunal680@gmail.com>
2018-03-27 11:46:54 +0300

    removed changes from parent

--- src/test/java/ch/uzh/ifi/seal/changedistiller/unittest/AdditionalObjectStateTest.java
+++ src/test/java/ch/uzh/ifi/seal/changedistiller/unittest/AdditionalObjectStateTest.java
@@ -9,9 +9,9 @@ package ch.uzh.ifi.seal.changedistiller.unittest;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,6 +19,7 @@ package ch.uzh.ifi.seal.changedistiller.unittest;
  * limitations under the License.
  * #L%
  */
+
 
 import static org.junit.Assert.assertEquals;
 
@@ -31,12 +32,12 @@ import ch.uzh.ifi.seal.changedistiller.model.entities.SourceCodeChange;
 
 public class AdditionalObjectStateTest {
 	List<SourceCodeChange> sourceCodeChangeList;
-	
+
 	@Before
 	public void setUp() {
 		sourceCodeChangeList = FileDistillerUtil.getChangesFromFile("AdditionalObjectState_Left.java", "AdditionalObjectState_Right.java");
 	}
-	
+
 	@Test
 	public void classRenamingTest() {
 		String expected = "ADDITIONAL_OBJECT_STATE\n";
@@ -45,7 +46,7 @@ public class AdditionalObjectStateTest {
 		for(SourceCodeChange change : sourceCodeChangeList) {
 			stringBuilder.append(change.getLabel() + "\n");
 		}
-		
+
 		assertEquals(stringBuilder.toString(), expected);
 	}
 }
--- src/test/java/ch/uzh/ifi/seal/changedistiller/unittest/FileDistillerUtil.java
+++ src/test/java/ch/uzh/ifi/seal/changedistiller/unittest/FileDistillerUtil.java
@@ -9,9 +9,9 @@ package ch.uzh.ifi.seal.changedistiller.unittest;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,6 +19,7 @@ package ch.uzh.ifi.seal.changedistiller.unittest;
  * limitations under the License.
  * #L%
  */
+
 
 import java.io.File;
 import java.net.URL;
@@ -36,7 +37,7 @@ public class FileDistillerUtil {
 		Path resourceDirectory = Paths.get("resources","testdata","src_changetypes");
 		File left = new File(resourceDirectory + "/" + leftFileName);
 		File right = new File(resourceDirectory + "/" + rightFileName);
-		
+
 		FileDistiller distiller = ChangeDistiller.createFileDistiller(Language.JAVA);
 		try {
 			distiller.extractClassifiedSourceCodeChanges(left, right);
