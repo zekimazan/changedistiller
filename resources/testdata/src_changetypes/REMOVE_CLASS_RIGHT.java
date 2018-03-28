c05bcdb5560372f85e699c845fe60b807595585a
guyaniker <gguyaniker@gmail.com>
2018-03-28 11:31:06 -0700

    Added Java Files to be tested #34

---
+++ resources/testdata/src_changetypes/REMOVE_CLASS_RIGHT.java
@@ -0,0 +1,35 @@
+
+public class REMOVED_CLASS_RIGHT {
+	
+	public class Cinema {
+		
+		private String[] movieNames;
+		private int[] saloonNumbers;
+		private int capacity;
+		private int saloonCap;
+	
+		public Cinema(int cap)
+		{
+			movieNames = new String[cap];
+			saloonNumbers = new int[cap];
+			this.capacity = cap;
+		}
+		
+		public void fillSaloon() {
+			for(int i = 0; i < this.capacity;i++)
+				saloonNumbers[i] = i+1;
+		}
+		public void setMovie(int saloon,String movieName)
+		{
+			this.movieNames[saloon] = movieName;
+		}
+		public String getMovieName(int saloon)
+		{
+			if(movieNames[saloon] != null)
+				return movieNames[saloon];
+			else
+				return "Empty";
+		}
+	}
+}
+
