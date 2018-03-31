b2bbf1d61896a94b114e92f6ce9de2d2e69f592d
ubozdogan <umtbzdgn@gmail.com>
2018-03-31 20:12:41 +0300

    DocInsertRight, COmmentUpdateleft,COmmentUpdateRig
    Added some of the classes

---
+++ COMMENT_UPDATELeft.java
@@ -0,0 +1,13 @@
+package test;
+public class COMMENT_UPDATELeft {
+
+	system.out.println("asd");
+	int a=6;
+	public void seta(int term) {
+		this.a=term;
+	}
+	public int geta() {
+		return a;
+	}
+	//COmment update left class
+}
