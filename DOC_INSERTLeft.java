5270be5eef95749a7dc14ef9bc5ec4ac94cd5cae
ubozdogan <umtbzdgn@gmail.com>
2018-03-31 20:14:15 +0300

    Added some of the test classes
    Test classes addition

---
+++ DOC_INSERTLeft.java
@@ -0,0 +1,20 @@
+
+public class DOC_INSERTLeft {
+	
+
+	/**
+	 * @param i
+	 * @return
+	 */
+	public int doAction(int i) {
+	    return i;
+	}
+	
+	/**
+	 * @param s
+	 * @return
+	 */
+	public void UmutBozdogan(String s) {
+		return s;
+	}
+}
