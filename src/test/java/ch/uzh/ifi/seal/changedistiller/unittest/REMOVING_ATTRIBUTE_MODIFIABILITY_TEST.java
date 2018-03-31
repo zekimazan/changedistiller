8ecade69af98d825695005419245731012554374
guyaniker <gguyaniker@gmail.com>
2018-03-31 08:16:31 -0700

    Added Unit Test. Closes #37

---
+++ src/test/java/ch/uzh/ifi/seal/changedistiller/unittest/REMOVING_ATTRIBUTE_MODIFIABILITY_TEST.java
@@ -0,0 +1,51 @@
+package ch.uzh.ifi.seal.changedistiller.unittest;
+
+/*
+ * #%L
+ * ChangeDistiller
+ * %%
+ * Copyright (C) 2011 - 2018 Software Architecture and Evolution Lab, Department of Informatics, UZH
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import ch.uzh.ifi.seal.changedistiller.model.entities.SourceCodeChange;
+
+public class REMOVING_ATTRIBUTE_MODIFIABILITY_TEST {
+	List<SourceCodeChange> sourceCodeChangeList;
+	
+	@Before
+	public void setUp() {
+		sourceCodeChangeList = FileDistillerUtil.getChangesFromFile("REMOVING_ATTRIBUTE_MODIFIABILITY_LEFT.java", "REMOVING_ATTRIBUTE_MODIFIABILITY_RIGHT.java");
+	}
+	
+	@Test
+	public void classRenamingTest() {
+		String expected = "REMOVING_ATTRIBUTE_MODIFIABILITY\n";
+		
+		StringBuilder stringBuilder = new StringBuilder();
+		for(SourceCodeChange change : sourceCodeChangeList) {
+			stringBuilder.append(change.getLabel() + "\n");
+		}
+		
+		assertEquals(stringBuilder.toString(), expected);
+	}
+}
