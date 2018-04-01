cc781be984eacca8fb75677f449e7dc6c31d7a37
canberksakarya <canberksakarya@gmail.com>
2018-04-01 21:54:42 +0300

    Added Tests

---
+++ src/test/java/ch/uzh/ifi/seal/changedistiller/unittest/ParameterInsertTest.java
@@ -0,0 +1,31 @@
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
+import ch.uzh.ifi.seal.changedistiller.model.entities.SourceCodeChange;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ParameterInsertTest {
+    List<SourceCodeChange> sourceCodeChangeList;
+
+
+    @Before
+    public void setUp() {
+        sourceCodeChangeList = FileDistillerUtil.getChangesFromFile("ParameterInsert_Left.java", "ParameterInsert_Right.java");
+
+    }
+
+    @Test
+    public void parameterInsertTest() {
+        String expected = "PARAMETER_INSERT\n";
+
+        StringBuilder stringBuilder = new StringBuilder();
+        for(SourceCodeChange change : sourceCodeChangeList) {
+            stringBuilder.append(change.getLabel() + "\n");
+        }
+
+        assertEquals(stringBuilder.toString(), expected);
+    }
+
+}
