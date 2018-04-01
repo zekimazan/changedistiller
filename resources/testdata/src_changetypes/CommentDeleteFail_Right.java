acf1811f67bd541ee715df4079d97a6ad6ee4a46
Baran Sorgucu <aa0198420@gmail.com>
2018-04-01 23:17:40 +0300

    Teslim (#80)
    * Add testdata files for #12#18
    
    * Add unittests for CommentDelete and DocDelete
    
    * Change failing tests as AssertEquals

---
+++ resources/testdata/src_changetypes/CommentDeleteFail_Right.java
@@ -0,0 +1,14 @@
+/**
+ * A class with comments.
+ *
+ * @author Beat Fluri
+ */
+public class ClassWithComments {
+
+    public ClassWithComments() {
+        // a simple method
+        toString();
+    }
+    
+    /* no more methods */
+}
---
+++ src/test/java/ch/uzh/ifi/seal/changedistiller/unittest/CommentDeleteTest.java
@@ -0,0 +1,70 @@
+package ch.uzh.ifi.seal.changedistiller.unittest;
+
+/*
+ * #%L
+ * ChangeDistiller
+ * %%
+ * Copyright (C) 2011 - 2018 Software Architecture and Evolution Lab, Department of Informatics, UZH
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import ch.uzh.ifi.seal.changedistiller.model.entities.SourceCodeChange;
+
+public class CommentDeleteTest {
+	List<SourceCodeChange> sourceCodeChangeList, sourceCodeChangeList2;
+	
+	@Before
+	public void setUp() {
+		sourceCodeChangeList = FileDistillerUtil.getChangesFromFile("CommentDelete_Left.java", "CommentDelete_Right.java");
+	}
+	
+	@Before
+	public void setUp2() {
+		sourceCodeChangeList2 = FileDistillerUtil.getChangesFromFile("CommentDeleteFail_Left.java", "CommentDeleteFail_Right.java");
+	}
+	
+	@Test
+	public void commentDeleteTest() {
+		String expected = "COMMENT_DELETE\n";
+		
+		StringBuilder stringBuilder = new StringBuilder();
+		for(SourceCodeChange change : sourceCodeChangeList) {
+			stringBuilder.append(change.getLabel() + "\n");
+		}
+		
+		assertEquals(stringBuilder.toString(), expected);
+	}
+	
+	@Test
+	public void commentDeleteTestFail() {
+		String expected = "COMMENT_DELETE\n";
+		
+		StringBuilder stringBuilder = new StringBuilder();
+		for(SourceCodeChange change : sourceCodeChangeList) {
+			stringBuilder.append(change.getLabel() + "\n");
+		}
+		
+		assertEquals(stringBuilder.toString(), expected);
+	}
+}
