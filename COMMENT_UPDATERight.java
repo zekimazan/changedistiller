526a58bdcecc268723a0082bb232e31be3a6e31a
ubozdogan <umtbzdgn@gmail.com>
2018-04-01 21:04:36 +0300

    Umut Bozdogan Project Pull Request (#73)
    * DocInsertRight, COmmentUpdateleft,COmmentUpdateRig
    
    Added some of the classes
    
    * Added some of the test classes
    
    Test classes addition
    
    * Test Method Renaming
    
    Gözümden kaçmış, geri dönüşünüz için teşekkür ederim.

---
+++ COMMENT_UPDATERight.java
@@ -0,0 +1,15 @@
+package test;
+public class COMMENT_UPDATERight {
+
+
+	system.out.println("asd");
+	int a=6;
+	public void seta(int term) {
+		this.a=term;
+	}
+	public int geta() {
+		return a;
+	}
+	//COmment update right class
+}
+
---
+++ DOC_INSERTRight.java
@@ -0,0 +1,24 @@
+
+public class DOC_INSERTRight {
+
+
+	
+
+	/**
+	 * @param k
+	 * @return
+	 */
+	public int doAction(long k) {
+	    return i;
+	}
+	
+	/**
+	 * @param j
+	 * @return
+	 */
+	public void UmutBozdogan(float j) {
+		return s;
+	}
+}
+
+
