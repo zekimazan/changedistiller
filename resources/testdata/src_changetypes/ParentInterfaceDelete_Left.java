23ebef3bffa01eee6b9af31d0202513920f75d0c
berk-12 <36491172+berk-12@users.noreply.github.com>
2018-04-02 21:47:11 +0300

    Update ParentInterfaceDelete_Left.java

--- resources/testdata/src_changetypes/ParentInterfaceDelete_Left.java
+++ resources/testdata/src_changetypes/ParentInterfaceDelete_Left.java
@@ -4,76 +4,71 @@ package test;
  * This is our first (left) test class.
  * @author Beat Fluri
  */
-public interface ParentInterfaceDelete_Left extends ParentInterfaceDelete_Right {
-	
-	/**
-	 * Yet another attribute
-	 */
-	public String aString = "test_string";
-	
-	/*
-	 * Scarab Lord Kungen
-	 */
-	public String aField;
+public class Test implements IInterface{
 
-	public static String sField;
-	public volatile int vField;
-	public transient String tField;
-	public synchronized long synchField;
-	
-	private String arrayField;
-	
-	public Integer anInteger = new Integer(1);
-	
-	/**
-	 * Yet another method with a comment
-	 * @param number
-	 * @return
-	 */
-	public int foo(int number) {
-		System.out.println("left");
-		
-		// check if number is greater than -1
-		boolean check = number > 0;
-		int a = 0;
-		int b = 2;
+    /*
+     * Scarab Lord Kungen
+     */
+    public String aField;
 
-		// check the huga number
-		// and some new
+    public static String sField;
+    public volatile int vField;
+    public transient String tField;
+    public synchronized long synchField;
 
-		if (check) {
-			/* This is the most beautiful comment in the world
-			 * and soon it will be gone :'(
-			 */
-			a = 23 + Integer.parseInt("42");
-			b = Math.round(Math.random() /* mimimi */);
-			return a + b;
-		} else {
-			/* huga bimbo */
-			b = Math.abs(number);
-			String.valueOf(true);
-			return b;
-		}
-	}
-	/*
-	 * Inner classes are cool
-	 */	
-	
-	private class Bar {
-		private void method() {
-			System.out.println();
-			System.out.println();
-			System.out.println();
-		}
-	}
-	// the huga bar method
-	public void bar(int test) {
-		System.out.println("aString");
-	}
+    private String arrayField;
 
-	public native void nativeMethod();
-	
-	public strictfp float strictfpMethod() {
-		return 2.0f * 3.3f;
-	}
+    public Integer anInteger = new Integer(1);
+
+    /**
+     * Yet another method with a comment
+     * @param number
+     * @return
+     */
+    public int foo(int number) {
+        System.out.println("left");
+
+        // check if number is greater than -1
+        boolean check = number > 0;
+        int a = 0;
+        int b = 2;
+
+        // check the huga number
+        // and some new
+
+        if (check) {
+            /* This is the most beautiful comment in the world
+             * and soon it will be gone :'(
+             */
+            a = 23 + Integer.parseInt("42");
+            b = Math.round(Math.random() /* mimimi */);
+            return a + b;
+        } else {
+            /* huga bimbo */
+            b = Math.abs(number);
+            String.valueOf(true);
+            return b;
+        }
+    }
+    /*
+     * Inner classes are cool
+     */
+
+    private class Bar {
+        private void method() {
+            System.out.println();
+            System.out.println();
+            System.out.println();
+        }
+    }
+    // the huga bar method
+    public void bar(int test) {
+        System.out.println("aString");
+    }
+
+    public native void nativeMethod();
+
+    public strictfp float strictfpMethod() {
+        return 2.0f * 3.3f;
+    }
 }
